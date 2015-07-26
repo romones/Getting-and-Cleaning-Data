4b39344c169147758bdbaf87b91cef6ed016fb7f
romones <roman.florea@gmail.com>
2015-07-26 16:24:41 +0300

    Fixed tidy set

--- run_analysis.R
+++ run_analysis.R
@@ -18,36 +18,42 @@ train_labels <- read.table("train/y_train.txt", stringsAsFactors = FALSE, col.na
 train_set <- read.table("train/X_train.txt", stringsAsFactors = FALSE)
 train_subjects <- read.table("train/subject_train.txt", stringsAsFactors = FALSE, col.names = "subjectid")
 
+
 #Merge sets
 merged_labels <- rbind(test_labels,train_labels)
 merged_set <- rbind(test_set,train_set)
 merged_subjects <- rbind(test_subjects,train_subjects)
 
-rm(list = ls()[grep("^(test|train).*$", ls(), perl = TRUE)]) #Clean  workspace from some heavy data
+rm(list = ls()[grep("^(test|train).*$", ls(), perl = TRUE)]) #Clean Workspace
 
-# Process the variable names according to tidy rules
-activities$name <- gsub("[[:punct:]]","",activities$name) # Remove the punctuation characters
+# Name activities properly
+activities$name <- gsub("[[:punct:]]","",activities$name)
 activities$name <- tolower(activities$name)
 
-activityname <- merge(merged_labels,activities, by=1)[,2] # Make meaningfull activity names
+activityname <- merge(merged_labels,activities, by=1)[,2]
 
 names(merged_set) <- features$name
 
-selectnames <- grep ("-mean\\(\\)|-std\\(\\)", features$name) # Get the columns of interest to be used with subsetting later 
+selectnames <- grep ("-mean\\(\\)|-std\\(\\)", features$name)
 
+#Tidy names
 names(merged_set) <- make.names(names = names(merged_set), unique = TRUE, allow_ = TRUE)
-
 
 merged_subset <- select(merged_set, selectnames)
 
-names(merged_subset) <- gsub("[[:punct:]]","",names(merged_subset)) # Remove any punctuation characters
+names(merged_subset) <- gsub("[[:punct:]]","",names(merged_subset))
 names(merged_subset) <- gsub("([[:upper:]])","-\\1",names(merged_subset)) # Add dashes for readability
 names(merged_subset) <- tolower(names(merged_subset))
 
-# Finally pack the final set
+# Pack the complete set
 merged_set_full <- cbind(merged_subjects,activityname,merged_subset)
 
-write.table(merged_set_full,"finalset.txt",row.names = FALSE)
+# Reshape the dataset for subject and activity
+shaped = melt(merged_set_full, id = c("subjectid", "activityname"))
 
-# Clean the workspace
+# Cast to make the final set
+tidy = dcast(shaped , subjectid + activityname ~ variable, mean)
+
+# Finally save the set
+write.table(merged_set_full,"finalset.txt",row.names = FALSE)
 rm(list = ls())
